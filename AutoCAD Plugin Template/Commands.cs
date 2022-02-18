6335ec8f3fc6bea0cb483db2c9d508b82f7a548c
Gilles Chanteau <gilecad@orange.fr>
2022-02-18 15:00:20 +0100

    Add files via upload

---
+++ AutoCAD Plugin Template/Commands.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using AcCoreAp = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+
+[assembly: CommandClass(typeof($safeprojectname$.Commands))]
+
+namespace $safeprojectname$
+{
+    public class Commands
+    {
+        [CommandMethod("TEST")]
+        public static void Test()
+        {
+            var doc = AcCoreAp.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
+            var ed = doc.Editor;
+        }
+    }
+}
---
+++ AutoCAD Plugin Template/Initialization.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using AcCoreAp = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+
+namespace $safeprojectname$
+{
+    public class Initialization : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            AcCoreAp.Idle += OnIdle;
+        }
+
+        private void OnIdle(object sender, EventArgs e)
+        {
+            var doc = AcCoreAp.DocumentManager.MdiActiveDocument;
+            if (doc != null)
+            {
+                AcAp.Idle -= OnIdle;
+                doc.Editor.WriteMessage("\n$projectname$ loaded.\n");
+            }
+        }
+
+        public void Terminate()
+        { }
+    }
+}
