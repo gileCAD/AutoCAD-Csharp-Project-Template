7e7475b164093979a91a690d87f7a7a89a0c140c
Gilles Chanteau <gilecad@orange.fr>
2024-03-26 20:47:22 +0100

    Add files via upload
    Add AutoCAD R25 (.NET 8) template

---
+++ AutoCAD R25 Csharp Plugin Template/Commands.cs
@@ -0,0 +1,19 @@
+﻿[assembly: CommandClass(typeof($safeprojectname$.Commands))]
+
+namespace $safeprojectname$
+{
+    public class Commands
+    {
+        [CommandMethod("Test")]
+        public static void Test()
+        {
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
+            var ed = doc.Editor;
+
+            using var tr = db.TransactionManager.StartTransaction();
+
+            tr.Commit();
+        }
+    }
+}
---
+++ AutoCAD R25 Csharp Plugin Template/ExtensionApplication.cs
@@ -0,0 +1,25 @@
+﻿[assembly: ExtensionApplication(typeof($safeprojectname$.ExtensionApplication))]
+
+namespace $safeprojectname$
+{
+    internal class ExtensionApplication : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            Application.Idle += OnIdle;
+        }
+
+        private void OnIdle(object? sender, EventArgs e)
+        {
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc != null)
+            {
+                Application.Idle -= OnIdle;
+                doc.Editor.WriteMessage("\n$safeprojectname$ loaded.\n");
+            }
+        }
+
+        public void Terminate()
+        { }
+    }
+}
